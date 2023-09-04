947044e112b908bb0f6d95d556cba49c90c49965
Surraiya Islam <i.surraiya@qa-academy.by>
2023-09-04 15:50:57 +0300

    Modified JobTitleServiceTest and created test data for each test separately

--- api/src/test/java/com/knits/enterprise/service/company/JobTitleServiceTest.java
+++ api/src/test/java/com/knits/enterprise/service/company/JobTitleServiceTest.java
@@ -1,8 +1,9 @@
 package com.knits.enterprise.service.company;
+import com.knits.enterprise.config.Constants;
 import com.knits.enterprise.dto.common.PaginatedResponseDto;
 import com.knits.enterprise.dto.company.JobTitleDto;
 import com.knits.enterprise.dto.search.JobTitleSearchDto;
-import com.knits.enterprise.exceptions.JobTitleException;
+import com.knits.enterprise.exceptions.UserException;
 import com.knits.enterprise.mapper.company.JobTitleMapper;
 import com.knits.enterprise.mapper.company.JobTitleMapperImpl;
 import com.knits.enterprise.model.company.JobTitle;
@@ -18,8 +19,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
-import java.time.LocalDateTime;
-import java.util.ArrayList;
+import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.*;
@@ -39,25 +39,15 @@ class JobTitleServiceTest {
     @InjectMocks
     private JobTitleService jobTitleService;
 
-    private JobTitleDto jobTitleDto;
-    private JobTitle jobTitle;
+    private User user;
     private String exceptionMessage;
     private Long id;
-    private List<JobTitle> jobTitleList;
-    private List<JobTitleDto> jobTitleDtoList;
 
     @BeforeEach
     void setUp() {
         exceptionMessage = "User#" + id + " not found";
         id = 2L;
-        jobTitleDto = JobTitleDto.builder()
-                .name("A test name")
-                .description("A test description")
-                .startDate(String.valueOf(LocalDateTime.now()))
-                .active(true)
-                .build();
-
-        User user = User.builder()
+        user = User.builder()
                 .id(1L)
                 .firstName("A test firstName")
                 .lastName("A test lastName")
@@ -65,12 +55,164 @@ class JobTitleServiceTest {
                 .active(true)
                 .email("test@test.com")
                 .build();
+    }
 
-        jobTitle = JobTitle.builder()
+    @Test
+    @DisplayName("create jobTitle success")
+    void saveNewJobTitle() {
+        JobTitleDto jobTitleDto = JobTitleDto.builder()
+                .name("A test name")
+                .description("A test description")
+                .startDate("04/09/2017")
+                .active(true)
+                .build();
+
+        when(jobTitleRepository.save(Mockito.any(JobTitle.class)))
+                .thenAnswer(invocation -> invocation.getArgument(0));
+
+        JobTitleDto savedDto = jobTitleService.saveNewJobTitle(jobTitleDto);
+
+        verify(jobTitleRepository).save(jobTitleArgumentCaptor.capture());
+        verify(jobTitleRepository, times(1)).save(jobTitleArgumentCaptor.getValue());
+        assertEquals(jobTitleDto, savedDto);
+    }
+
+    @Test
+    @DisplayName("Find JobTitle by Id - Success")
+    void findJobTitleById_Success() {
+        Long jobId = 1L;
+        JobTitle savedJobTitle = JobTitle.builder()
+                .id(jobId)
+                .name("A test name")
+                .description("A test description")
+                .startDate(LocalDate.parse("04/09/2023", Constants.DATE_FORMATTER))
+                .active(true)
+                .createdBy(user)
+                .build();
+
+        when(jobTitleRepository.findById(any())).thenReturn(Optional.of(savedJobTitle));
+
+        JobTitleDto foundJobTitle = jobTitleService.findJobTitleById(jobId);
+
+        verify(jobTitleRepository).findById(eq(jobId));
+        assertNotNull(foundJobTitle);
+        assertEquals(jobTitleMapper.toDto(savedJobTitle), foundJobTitle);
+    }
+
+    @Test
+    @DisplayName("find jobTitle by Id fail : is not found")
+    void findJobTitleByIdFail() {
+        JobTitle jobTitle = JobTitle.builder()
                 .id(1L)
                 .name("A test name")
                 .description("A test description")
-                .startDate(LocalDateTime.now())
+                .startDate(LocalDate.parse("04/09/2023", Constants.DATE_FORMATTER))
+                .active(true)
+                .createdBy(user)
+                .build();
+
+        when(jobTitleRepository.findById(any())).thenThrow(new UserException(exceptionMessage));
+        UserException exception = assertThrows(UserException.class,
+                () -> jobTitleService.findJobTitleById(jobTitle.getId()));
+        assertEquals(exceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Update JobTitle - Success")
+    void partialUpdate_Success() {
+        Long jobId = 1L;
+        String updatedDescription = "Updated job description";
+        JobTitleDto jobTitleDto = JobTitleDto.builder()
+                .description(updatedDescription)
+                .build();
+        JobTitle savedJobTitle = JobTitle.builder()
+                .id(jobId)
+                .name("A test name")
+                .description("A test description")
+                .startDate(LocalDate.parse("04/09/2023", Constants.DATE_FORMATTER))
+                .active(true)
+                .createdBy(user)
+                .build();
+
+        when(jobTitleRepository.findById(any())).thenReturn(Optional.of(savedJobTitle));
+        when(jobTitleRepository.save(any(JobTitle.class))).thenReturn(savedJobTitle);
+
+        JobTitleDto updatedJobTitle = jobTitleService.partialUpdate(jobId, jobTitleDto);
+
+        verify(jobTitleRepository).findById(eq(jobId));
+        verify(jobTitleRepository).save(jobTitleArgumentCaptor.capture());
+
+        JobTitle updatedJobTitleEntity = jobTitleArgumentCaptor.getValue();
+        assertEquals(updatedDescription, updatedJobTitleEntity.getDescription());
+        assertEquals(jobTitleDto, updatedJobTitle);
+    }
+
+    @Test
+    @DisplayName("update jobTitle fail : id not found")
+    void partialUpdateFail() {
+        Long jobId = 1L;
+        String updatedDescription = "Updated job description";
+        JobTitleDto jobTitleDto = JobTitleDto.builder()
+                .description(updatedDescription)
+                .build();
+
+        when(jobTitleRepository.findById(eq(jobId))).thenThrow(new UserException(exceptionMessage));
+        UserException exception = assertThrows(UserException.class,
+                () -> jobTitleService.partialUpdate(jobId, jobTitleDto));
+
+        assertEquals(exceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("delete jobTitle by id success")
+    void deleteJobTitleSuccess() {
+        JobTitle jobTitle = JobTitle.builder()
+                .id(1L)
+                .name("A test name")
+                .description("A test description")
+                .startDate(LocalDate.parse("04/09/2023", Constants.DATE_FORMATTER))
+                .active(true)
+                .createdBy(user)
+                .build();
+        when(jobTitleRepository.findById(any())).thenReturn(Optional.of(jobTitle));
+        jobTitleService.delete(jobTitle.getId());
+
+        verify(jobTitleRepository, times(1)).findById(jobTitle.getId());
+        verify(jobTitleRepository, times(1)).delete(jobTitle);
+    }
+
+    @Test
+    @DisplayName("Delete JobTitle by ID - Fail: ID Not Found")
+    void deleteJobTitleByIdFail_IdNotFound() {
+        when(jobTitleRepository.findById(any())).thenThrow(new UserException(exceptionMessage));
+
+        UserException exception = assertThrows(UserException.class,
+                () -> jobTitleService.delete(1L));
+
+        assertEquals(exceptionMessage, exception.getMessage());
+    }
+
+    @Test
+    @DisplayName("Search JobTitles by Pagination and sorting - Success")
+    void searchJobTitles() {
+        JobTitleDto firstJobTitleDto = JobTitleDto.builder()
+                .name("A test name")
+                .description("A test description")
+                .startDate("04/09/2023")
+                .active(true)
+                .build();
+
+        JobTitleDto secondJobTitleDto = JobTitleDto.builder()
+                .name("A second test name")
+                .description("A second test description")
+                .startDate("04/09/2023")
+                .build();
+
+        JobTitle firstJobTitle = JobTitle.builder()
+                .id(1L)
+                .name("A test name")
+                .description("A test description")
+                .startDate(LocalDate.parse("04/09/2023", Constants.DATE_FORMATTER))
                 .active(true)
                 .createdBy(user)
                 .build();
@@ -79,105 +221,13 @@ class JobTitleServiceTest {
                 .id(1L)
                 .name("A second test name")
                 .description("A second test description")
-                .startDate(LocalDateTime.now())
+                .startDate(LocalDate.parse("04/09/2023", Constants.DATE_FORMATTER))
                 .active(true)
                 .createdBy(user)
                 .build();
 
-        JobTitleDto secondJobTitleDto = JobTitleDto.builder()
-                .name("A second test name")
-                .description("A second test description")
-                .startDate(String.valueOf(LocalDateTime.now()))
-                .build();
-
-        jobTitleList = new ArrayList<>();
-        jobTitleList.add(jobTitle);
-        jobTitleList.add(secondJobTitle);
-
-        jobTitleDtoList = new ArrayList<>();
-        jobTitleDtoList.add(jobTitleDto);
-        jobTitleDtoList.add(secondJobTitleDto);
-    }
-
-    @Test
-    @DisplayName("create jobTitle success")
-    void saveNewJobTitle() {
-
-        when(jobTitleMapper.toEntity(any())).thenReturn(jobTitle);
-        when(jobTitleRepository.save(any())).thenReturn(jobTitle);
-        when(jobTitleMapper.toDto(any())).thenReturn(jobTitleDto);
-        JobTitleDto savedDto = jobTitleService.saveNewJobTitle(jobTitleDto);
-        verify(jobTitleRepository).save(jobTitleArgumentCaptor.capture());
-        JobTitle toSaveEntity = jobTitleArgumentCaptor.getValue();
-        verify(jobTitleMapper, times(1)).toDto(toSaveEntity);
-        verify(jobTitleMapper, times(1)).toEntity(jobTitleDto);
-        verify(jobTitleRepository, times(1)).save(toSaveEntity);
-        assertEquals(jobTitleDto, savedDto);
-    }
-
-    @Test
-    @DisplayName("find jobTitle by Id success")
-    void findJobTitleById() {
-        when(jobTitleRepository.findById(any())).thenReturn(Optional.of(jobTitle));
-        JobTitleDto jobTitleById = jobTitleService.findJobTitleById(jobTitle.getId());
-        verify(jobTitleRepository).findById(idArgumentCaptor.capture());
-        Long id = idArgumentCaptor.getValue();
-        assertEquals(jobTitle.getId(), id);
-        assertEquals(jobTitleDto, jobTitleById);
-    }
-
-    @Test
-    @DisplayName("find jobTitle by Id fail : is not found")
-    void findJobTitleByIdFail() {
-        when(jobTitleRepository.findById(any())).thenThrow(new JobTitleException(exceptionMessage));
-        JobTitleException exception = assertThrows(JobTitleException.class,
-                () -> jobTitleService.findJobTitleById(jobTitle.getId()));
-        assertEquals(exceptionMessage, exception.getMessage());
-    }
-
-    @Test
-    @DisplayName("update jobTitle success")
-    void partialUpdate() {
-        when(jobTitleRepository.findById(any())).thenReturn(Optional.of(jobTitle));
-        when(jobTitleRepository.save(any())).thenReturn(jobTitle);
-        when(jobTitleMapper.toDto(any())).thenReturn(jobTitleDto);
-        JobTitleDto updatedJobTitle = jobTitleService.partialUpdate(jobTitle.getId(), jobTitleDto);
-        verify(jobTitleRepository).findById(idArgumentCaptor.capture());
-        verify(jobTitleRepository).save(jobTitleArgumentCaptor.capture());
-        JobTitle value = jobTitleArgumentCaptor.getValue();
-        verify(jobTitleMapper).toDto(value);
-        assertEquals(jobTitleDto, updatedJobTitle);
-    }
-
-    @Test
-    @DisplayName("update jobTitle fail : id not found")
-    void partialUpdateFail() {
-        when(jobTitleRepository.findById(any())).thenThrow(new JobTitleException(exceptionMessage));
-        JobTitleException exception = assertThrows(JobTitleException.class,
-                () -> jobTitleService.partialUpdate(jobTitleDto.getId(), jobTitleDto));
-        assertEquals(exceptionMessage, exception.getMessage());
-    }
-
-    @Test
-    @DisplayName("delete jobTitle by id success")
-    void deleteJobTitleSuccess() {
-        when(jobTitleRepository.findById(any())).thenReturn(Optional.of(jobTitle));
-        jobTitleService.deleteJobTitle(jobTitle.getId());
-        verify(jobTitleRepository, times(1)).findById(jobTitle.getId());
-        verify(jobTitleRepository, times(1)).delete(jobTitle);
-    }
-
-    @Test
-    @DisplayName("delete jobTitle by id fail : id not found")
-    void deleteJobTitleFail() {
-        when(jobTitleRepository.findById(any())).thenThrow(new JobTitleException(exceptionMessage));
-        JobTitleException exception = assertThrows(JobTitleException.class,
-                () -> jobTitleService.deleteJobTitle(jobTitle.getId()));
-        assertEquals(exceptionMessage, exception.getMessage());
-    }
-
-    @Test
-    void searchJobTitles() {
+        List<JobTitle> jobTitleList = List.of(firstJobTitle, secondJobTitle);
+        List<JobTitleDto> jobTitleDtoList = List.of(firstJobTitleDto, secondJobTitleDto);
 
         Page<JobTitle> jobTitlePage = new PageImpl<>(jobTitleList);
         JobTitleSearchDto searchDto = new JobTitleSearchDto();
@@ -185,7 +235,9 @@ class JobTitleServiceTest {
 
         when(jobTitleRepository.findAll(ArgumentMatchers.<Specification<JobTitle>>any(), (Pageable) any())).thenReturn(jobTitlePage);
         when(jobTitleMapper.toDtos(jobTitlePage.getContent())).thenReturn(jobTitleDtoList);
-        PaginatedResponseDto<JobTitleDto> allJobTitles = jobTitleService.findJobTitlesBySortingAndPagination(searchDto);
+
+        PaginatedResponseDto<JobTitleDto> allJobTitles = jobTitleService.findBySortingAndPagination(searchDto);
+
         verify(jobTitleRepository).findAll(ArgumentMatchers.<Specification<JobTitle>>any(), (Pageable) any());
         verify(jobTitleMapper, times(2)).toDtos(jobTitlePage.getContent());
 
@@ -193,7 +245,21 @@ class JobTitleServiceTest {
         assertEquals(searchDto.getDir().name(), allJobTitles.getSortDirection());
         assertEquals(searchDto.getSort(), allJobTitles.getSortingFields());
         assertEquals(searchDto.getLimit(), allJobTitles.getSize());
-        assertEquals(jobTitleDtoList.size(), allJobTitles.getSize());
         assertEquals(jobTitleDtoList, allJobTitles.getData());
+    }
+
+    @Test
+    @DisplayName("Search JobTitles by Pagination and Sorting - Failure")
+    void searchJobTitlesFailure() {
+        JobTitleSearchDto searchDto = new JobTitleSearchDto();
+        searchDto.setLimit(2);
+
+        when(jobTitleRepository.findAll(ArgumentMatchers.<Specification<JobTitle>>any(), (Pageable) any()))
+                .thenThrow(new RuntimeException("Failed to retrieve job titles"));
+
+        RuntimeException exception = assertThrows(RuntimeException.class,
+                () -> jobTitleService.findBySortingAndPagination(searchDto));
+
+        assertEquals("Failed to retrieve job titles", exception.getMessage());
     }
 }
